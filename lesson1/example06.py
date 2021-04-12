ac32a0cf2a7c37459be816f5e4f775aa7de59faa
MaxiFoxy <78953942+MaxiFoxy@users.noreply.github.com>
2021-04-12 22:57:02 +0300

    6

--- lesson1/example06.py
+++ lesson1/example06.py
@@ -1,5 +1,10 @@
-A=int(input("Введите a: "))
-B=int(input("Введите b: "))
-while A <= B:
-    A=A+A*0.1
-    print(float(A))
+A = int(input("Введите 1-й результат: "))
+B = int(input("Введите желаемый результат: "))
+i: int = 1
+print(f"{i}-й день: ", float(A))
+while True:
+    A = A + A * 0.1
+    i = i + 1
+    print(f"{i}-й день: ", float(A))
+    if A >= B:
+        break
