836038042e5af049f3d070a3063f75a3145610e5
MaxiFoxy <78953942+MaxiFoxy@users.noreply.github.com>
2021-04-14 11:39:39 +0300

    3
    3

--- lesson2/example03.py
+++ lesson2/example03.py
@@ -1,7 +1,8 @@
 def monthname(nummonth):
-    month = ("Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь")
-    month = month[nummonth-1]
+    month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+    month = month[nummonth - 1]
     return month
+
 
 print("1 способ")
 Seasons = {"Зима": (12, 1, 2), "Весна": (3, 4, 5), "Лето": (6, 7, 8), "Осень": (9, 10, 11)}
@@ -37,9 +38,9 @@ while True:
     if 1 <= NumMonth <= 12:
         if NumMonth == 12:
             NumMonth = 0
-        SeasonsName=int(NumMonth/3)
+        SeasonsName = int(NumMonth / 3)
         SeasonsName = list_seasons[SeasonsName]
-        #месяц/3 и 12 прировнять к 0, обрезаем всё после запятой и получим ключ
+        # месяц/3 и 12 прировнять к 0, обрезаем всё после запятой и получим ключ
         break
     else:
         print("Месяцов всего 12")
