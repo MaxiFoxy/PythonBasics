a353162baaec7c2a62f3fd1edfa44215a8b3cbe8
MaxiFoxy <78953942+MaxiFoxy@users.noreply.github.com>
2021-04-14 13:58:09 +0300

    5
    5

---
+++ lesson2/example05.py
@@ -0,0 +1,11 @@
+Rating=[]
+while True:
+    print("Для выхода введите любое значени кроме цифр")
+    try:
+        RatingInput = int(input("Введите число: "))
+        Rating.append(RatingInput)
+        Rating = sorted(Rating)
+        Rating.reverse()
+        print("Пользователь ввел число:", RatingInput, "Результат", Rating)
+    except ValueError:
+        break
