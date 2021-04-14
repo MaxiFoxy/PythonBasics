3934783d0dc63f14ab85a564bd88b992c2d44c03
MaxiFoxy <78953942+MaxiFoxy@users.noreply.github.com>
2021-04-14 12:59:52 +0300

    4
    4

--- lesson2/example04.py
+++ lesson2/example04.py
@@ -1,1 +1,12 @@
-string = input("Введите слова")
+string = input("Введите слова: ")
+n = 0
+while True:
+    try:
+        s = n
+        # print(string[s:string.index(" ",n)]) # вывод полной строки
+        print(string[n:n + 10 if len(string[s:string.index(" ", n)]) >= 10 else string.index(" ", n)])  # если значений больше 10 то просто к значению n прибовляем 10 иначе смотри где следующий пробел
+        # print(string.index(" ",n))# ищем пробел от индекса n
+        n = string.index(" ", n) + 1  # прибовляем к найденому индексу 1 что бы он не зацыклился
+    except ValueError:
+        print(string[s:s + 10])
+        break
