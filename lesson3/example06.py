be2ca92e32d3509ddbe32d1c68641c7d03b0a378
MaxiFoxy <78953942+MaxiFoxy@users.noreply.github.com>
2021-05-18 11:25:50 +0300

    6

--- lesson3/example06.py
+++ lesson3/example06.py
@@ -1,33 +1,3 @@
-def explode(separator, string, limit=0):
-    """
-    Переделывает строку в массив через разделяемый символ
-    :param separator: Разделитель
-    :param string: Строка
-    :param limit: количество значений
-    :return: Ввыводит массив
-    """
-    run = []
-    n = 0
-    i = 0
-    try:
-        limit=int(limit)
-    except ValueError:
-        print("Значение limit должнобыть типа INT")
-    while True:
-        if limit <= i and limit > 0:
-            break
-        try:
-            run.append(string[n:string.index(separator, n)].strip())
-            n = string.index(separator, n) + 1
-        except ValueError:
-            run.append(string[n:].strip())
-            break
-        i+=1
-    separator = None
-    string = None
-    limit = None
-    return run
-
 def int_func0(text): #такой себе вариант так как при большой букве он будет менять на другой символ
     letter = chr(ord(text[0]) - 32)
     print(letter)
@@ -42,7 +12,7 @@ def int_func(text):
     else:
         return text
 
-run = explode(" ", input("Введител текст: "))
+run = input("Введител текст: ").split(' ')
 str=""
 for i in run:
     str+=int_func(i)+" "
