06f58439634824f220109481932ae80f48ea1714
BuildTools <unconfigured@null.spigotmc.org>
2021-06-12 15:19:41 +0300

    +

--- lesson4/example01.py
+++ lesson4/example01.py
@@ -1,1 +1,11 @@
-хуета
+
+def sal():
+    try:
+        time = float(input('Выработка в часах '))
+        salary = int(input('Ставка в у.е. '))
+        bonus = int(input('Премия в у.е. '))
+        res = time * salary + bonus
+        print(f'заработная плата сотрудника  {res}')
+    except ValueError:
+        return print('Not a number')
+sal()
